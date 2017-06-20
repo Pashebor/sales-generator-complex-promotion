032b141a307141b9048f48781b24a6234bc230cf
pashebor <pashebor@gmail.com>
2017-06-21 01:21:37 +0300

    Header screen has been done except mobile adaptivity

--- app/components/HeaderContent.js
+++ app/components/HeaderContent.js
@@ -42,19 +42,20 @@ class HeaderContent extends Component{
             <section className="header-content-background">
                 <div className="content--background">
                     <div className="container header__content">
-                        <h2 className="header__content-title">Профессиональный аудит сайта <span>за 72 часа</span></h2>
-                        <p className="header__content-text">Позволит увеличить число клиентов в несколько раз</p>
-                        <p className="header__content-text">с помощью наших рекомендаций по 47 критериям</p>
+                        <h2 className="header__content-title">КОМПЛЕКСНОЕ ПРОДВИЖЕНИЕ САЙТА В <span>ТОП-3</span> ЯНДЕКСА И GOOGLE</h2>
+                        <h3 className="header__content-subtitle">Гарантированно приведем на Ваш сайт тысячи новых клиентов</h3>
+                        <p className="header__content-text">Финансовая гарантия результата. Рост продаж. Продвижение только по целевым запросам.</p>
+                        <p className="header__content-text">Снижение стоимости привлечения клиента. Просчет окупаемости.</p>
                         <h5 className="know-form__title">Узнайте, сколько новых клиентов Вы можете получить с сайта</h5>
                         {this.clientsNotification()}
                         <form className="form-group know-form" onSubmit={this.btnSubmitHandler.bind(this)}>
                             <MaskedInput mask="+7(111) 111 11 11" type="text" ref="phone" className="form-control" placeholder="Телефон *"  required/>
-                            <input type="submit" className="btn submit-btn"  value="Узнать!"/>
+                            <input type="submit" className="btn submit-btn"  value="Узнать сколько получу клиентов!"/>
                         </form>
                     </div>
-                    <div className="separatorbottom">
-                        <div className="bigcircle-bottom"></div>
-                    </div>
+                </div>
+                <div className="separatorbottom">
+                    <svg version="1.1" id="bottom-triangle" viewBox="0 0 100 100" preserveAspectRatio="none"><path d="M 0 100 H 100 V 0 H 58.5 L 50 100 L 41.5 0 H 0 Z"></path></svg>
                 </div>
             </section>
         );
