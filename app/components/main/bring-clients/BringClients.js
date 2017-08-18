37feca10b28145013bdf31effa6f1ff4181069ac
sales-generator <pashebor@salesgenerator.pro>
2017-08-18 09:16:58 +0300

    A new changes

--- app/components/main/bring-clients/BringClients.js
+++ app/components/main/bring-clients/BringClients.js
@@ -15,6 +15,7 @@ class BringClients extends React.Component{
 
         this.props.sendClientsCallback(formData);
         yaCounter44418460.reachGoal('POLUCHIT_PREDLOJENIE');
+        /*'oje'*/
     }
 
     clientsNotification() {
